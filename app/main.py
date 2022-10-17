25b44919096172370c93cbfb32ff94d718da9195
Henrik Kühnemann <hk@yellownape.se>
2022-10-17 10:46:16 +0200

    initial commit

---
+++ app/main.py
@@ -0,0 +1,294 @@
+"""
+App to listen to incoming calls and gather information about incoming traffic.
+v 0.0.0
+"""
+
+from collections import deque
+from datetime import datetime, timedelta
+from typing import List
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates/")
+
+
+app = FastAPI(openapi_url="")
+
+
+class HeaderReporter:
+    """
+    Class to hold recent call data
+    """
+
+    def __init__(self):
+        self.recent_records = deque(maxlen=10)
+
+    async def add_call(
+        self,
+        request: Request,
+        filename: str,
+        created_time: datetime = datetime.utcnow(),
+    ):
+        """
+        Parse and add incoming calls to deque
+
+        :param created_time:
+        :param request:
+        :param filename:
+        :return:
+        """
+        last_call = {
+            "created_time": created_time,
+            "headers": request.headers,
+            "method": request.method,
+            "body": await request.body(),
+            "file": filename,
+            "url": request.url,
+            "params": request.query_params,
+            "cookies": request.cookies,
+        }
+
+        self.recent_records.appendleft(last_call)
+
+    def get_stored(self) -> List[dict]:
+        """
+        Returns content of deque in as list.
+        :return:
+        """
+        return list(self.recent_records)
+
+    def clean_old_records(self, hours: int = 4):
+        """
+        Iterates current records and removes records that are older than hours.
+        :param hours:
+        :return:
+        """
+        now = datetime.utcnow()
+        remove = []
+        for record in self.recent_records:
+            if now - record["created_time"] > timedelta(hours=hours):
+                remove.append(record)
+        for record in remove:
+            self.recent_records.remove(record)
+
+    def reset_stored(self):
+        """
+        Clears stored records.
+        :return:
+        """
+        self.recent_records = deque(maxlen=10)
+
+
+header_reporter = HeaderReporter()
+
+
+@app.get("/", status_code=200, include_in_schema=False)
+async def listening():
+    """
+    Simple health check.
+    :return:
+    """
+    return "Listening!"
+
+
+@app.get(
+    "/recent_calls",
+    status_code=200,
+    include_in_schema=False,
+    response_class=HTMLResponse,
+)
+async def get_recent_calls(request: Request):
+    """
+    User-friendly display of data from last 10 calls.
+
+    :param request:
+    :return:
+    """
+    header_reporter.clean_old_records()
+
+    return templates.TemplateResponse(
+        "return_last.html",
+        {"request": request, "recent_calls": header_reporter.get_stored()},
+    )
+
+
+@app.post("/recent_calls", status_code=201, include_in_schema=False)
+async def reset_recent_calls():
+    """
+    Reset records.
+
+    :param request:
+    :return:
+    """
+    header_reporter.reset_stored()
+
+    return
+
+
+@app.get("/{filename}", status_code=200, include_in_schema=False)
+async def get_one_level(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.post("/{filename}", status_code=200, include_in_schema=False)
+async def post_one_level(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.put("/{filename}", status_code=200, include_in_schema=False)
+async def put_one_level(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.get("/{level_one}/{filename}", status_code=200, include_in_schema=False)
+async def get_two_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.post("/{level_one}/{filename}", status_code=200, include_in_schema=False)
+async def post_two_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.put("/{level_one}/{filename}", status_code=200, include_in_schema=False)
+async def put_two_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.get(
+    "/{level_one}/{level_two}/{filename}", status_code=200, include_in_schema=False
+)
+async def get_three_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.post(
+    "/{level_one}/{level_two}/{filename}", status_code=200, include_in_schema=False
+)
+async def post_three_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.put(
+    "/{level_one}/{level_two}/{filename}", status_code=200, include_in_schema=False
+)
+async def put_three_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.get(
+    "/{level_one}/{level_two}/{level_three}/{filename}",
+    status_code=200,
+    include_in_schema=False,
+)
+async def get_four_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.post(
+    "/{level_one}/{level_two}/{level_three}/{filename}",
+    status_code=200,
+    include_in_schema=False,
+)
+async def post_four_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+@app.put(
+    "/{level_one}/{level_two}/{level_three}/{filename}",
+    status_code=200,
+    include_in_schema=False,
+)
+async def put_four_levels(request: Request, filename: str):
+    """
+    Listener to lift call data.
+    :param request:
+    :param filename:
+    :return:
+    """
+    await header_reporter.add_call(request, filename)
+    return filename
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8008, log_level="info")
---
+++ tests/test_app.py
@@ -0,0 +1,172 @@
+"""tests"""
+from datetime import datetime, timedelta
+
+from fastapi.testclient import TestClient
+from pydantic import BaseModel, Field
+
+from app.main import HeaderReporter, app
+
+tc = TestClient(app)
+
+
+def test_fastapi_routes():
+    """basic tests of all routes and methods"""
+    response = tc.get("/1234")
+    assert response.status_code == 200
+
+    response = tc.put("/1234")
+    assert response.status_code == 200
+
+    response = tc.post("/1234")
+    assert response.status_code == 200
+
+    response = tc.get("/1234/abcd")
+    assert response.status_code == 200
+
+    response = tc.put("/1234/abcd")
+    assert response.status_code == 200
+
+    response = tc.post("/1234/abcd")
+    assert response.status_code == 200
+
+    response = tc.get("/1234/abcd/asdfg")
+    assert response.status_code == 200
+
+    response = tc.put("/1234/abcd/asdfg")
+    assert response.status_code == 200
+
+    response = tc.post("/1234/abcd/asdfg")
+    assert response.status_code == 200
+
+    response = tc.get("/1234/asdafa/asda")
+    assert response.status_code == 200
+
+    response = tc.put("/1234/asdafa/asda")
+    assert response.status_code == 200
+
+    response = tc.post("/1234/asdafa/asda")
+    assert response.status_code == 200
+
+    response = tc.get("/")
+    assert response.status_code == 200
+
+    response = tc.get("/recent_calls")
+    assert response.status_code == 200
+
+    response = tc.post("/recent_calls")
+    assert response.status_code == 201
+
+
+class MockRequest(BaseModel):
+    """
+    Class to mock incoming requests
+    """
+    headers: dict
+    method: str
+    url: str
+    query_params: list = Field(default_factory=list)
+    cookies: dict = Field(default_factory=dict)
+
+    async def body(self):
+        """
+        mock body
+        :return:
+        """
+        return "BODY CONTENTS"
+
+
+def _get_mock_request():
+    """helper to create mock requests"""
+    return MockRequest(
+        **{
+            "headers": {"header1": 1, "header2": 2},
+            "method": "POST",
+            "url": "request.url",
+        }
+    )
+
+
+async def test_header_reporter_clean_old():
+    """
+    test for iterating over existing items in list and removing aged ones.
+    :return:
+    """
+
+    h_r = HeaderReporter()
+    new_req = _get_mock_request()
+    old_req = _get_mock_request()
+    await h_r.add_call(new_req, filename="test_new")
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=5),
+    )
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=3),
+    )
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=8),
+    )
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=5),
+    )
+    await h_r.add_call(new_req, filename="test_new")
+    assert len(h_r.recent_records) == 6
+
+    h_r.clean_old_records()
+
+    assert len(h_r.recent_records) == 3
+
+
+async def test_header_reporter_reset():
+    """
+    Test for reseting stored incoming calls.
+    :return:
+    """
+    h_r = HeaderReporter()
+    new_req = _get_mock_request()
+    old_req = _get_mock_request()
+    await h_r.add_call(new_req, filename="test_new")
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=5),
+    )
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=3),
+    )
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=8),
+    )
+    await h_r.add_call(
+        old_req,
+        filename="test_old",
+        created_time=datetime.utcnow() - timedelta(hours=5),
+    )
+    await h_r.add_call(new_req, filename="test_new")
+    assert len(h_r.recent_records) == 6
+
+    h_r.reset_stored()
+
+    assert len(h_r.recent_records) == 0
+
+
+if __name__ == "__main__":
+    import asyncio
+    import os
+
+    os.chdir("../app")
+    test_fastapi_routes()
+
+    asyncio.run(test_header_reporter_clean_old())
+    asyncio.run(test_header_reporter_reset())
